ebf3702a1dc8a1805980dc11de4fe07a7bb9276e
Georgette Bashour <13901057@student.uts.edu.au>
2022-09-24 20:17:26 +1000

    Fixed all code errors, looping bounds and mirrored quadrant vertically

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject powerPellet = GameObject.Find("PowerPellet").GetComponent<GameObject>();
-    public GameObject outsideCorner = GameObject.Find("OutsideCorner").GetComponent<GameObject>();
-    public GameObject outsideWall = GameObject.Find("OutsideWall").GetComponent<GameObject>();
-    public GameObject insideCorner = GameObject.Find("InsideCorner").GetComponent<GameObject>();
-    public GameObject insideWall = GameObject.Find("InsideWall").GetComponent<GameObject>();
-    public GameObject normalPellet = GameObject.Find("NormalPellet").GetComponent<GameObject>();
-    public GameObject junction = GameObject.Find("Junction").GetComponent<GameObject>();
+    public GameObject powerPellet;
+    public GameObject outsideCorner;
+    public GameObject outsideWall;
+    public GameObject insideCorner;
+    public GameObject insideWall;
+    public GameObject normalPellet;
+    public GameObject junction;
 
-    public Grid grid = GameObject.Find("Grid").GetComponent<Grid>(); // Grid to clear? Tilemap?
+    public GameObject grid; // Grid to clear? Tilemap?
 
     int[,] levelMap =
         {
@@ -36,47 +36,63 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        powerPellet = GameObject.Find("PowerPellet");
+        outsideCorner = GameObject.Find("OutsideCorner");
+        outsideWall = GameObject.Find("OutsideWall");
+        insideCorner = GameObject.Find("InsideCorner");
+        insideWall = GameObject.Find("InsideWall");
+        normalPellet = GameObject.Find("NormalPellet");
+        junction = GameObject.Find("Junction");
+
+        grid = GameObject.Find("Grid"); // Grid to clear? Tilemap?
+
         // Delete the existing Level 1 Scene -- i.e clear Tilemap
         Destroy(grid);
         // Procedurally Generate Level:
         // Variable for keeping track of position
         Vector3 placement = new Vector3(-5.5f, 4.5f, 0.0f); // position of top-left corner
-        for (int i = 0; i < levelMap.Length; i++) // nested for loop to iterate through 2D array
+        for (int i = 0; i < 15; i++) // nested for loop to iterate through 2D array
         {
-            for (int j = 0; j < levelMap.Length; j++)
+            for (int j = 0; j < 14; j++)
             {
                 if (levelMap[i, j] == 1)
                 {
                     GameObject outsideCornerClone = Instantiate(outsideCorner, placement, Quaternion.identity);
-                    //outsideCornerClone.transform.localScale -- scale back to 1,1,1 ?
+                    outsideCornerClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                     //outsideCornerClone.transform.Rotate(0.0f, 0.0f, 180.0f);
                 }
                 else if (levelMap[i, j] == 2)
                 {
                     GameObject outsideWallClone = Instantiate(outsideWall, placement, Quaternion.identity);
+                    outsideWallClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                     //outsideWallClone.transform.Rotate(0.0f, 0.0f, 180.0f);
                 }
                 else if (levelMap[i, j] == 3)
                 {
                     GameObject insideCornerClone = Instantiate(insideCorner, placement, Quaternion.identity);
+                    insideCornerClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                     //insideCornerClone.transform.Rotate(0.0f, 0.0f, 180.0f);
                 }
                 else if (levelMap[i, j] == 4)
                 {
                     GameObject insideWallClone = Instantiate(insideWall, placement, Quaternion.identity);
+                    insideWallClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                     //insideWallClone.transform.Rotate(0.0f, 0.0f, 180.0f);
                 }
                 else if (levelMap[i, j] == 5)
                 {
                     GameObject normalPelletClone = Instantiate(normalPellet, placement, Quaternion.identity); // no rotation
+                    normalPelletClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                 }
                 else if (levelMap[i, j] == 6)
                 {
-                    GameObject powerPelletClone = Instantiate(powerPellet, placement, Quaternion.identity);
+                    GameObject powerPelletClone = Instantiate(powerPellet, placement, Quaternion.identity); // no rotation
+                    powerPelletClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                 }
                 else if (levelMap[i, j] == 7)
                 {
                     GameObject junctionClone = Instantiate(junction, placement, Quaternion.identity);
+                    junctionClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
                     //junctionClone.transform.Rotate(0.0f, 0.0f, 270.0f);
                 }
                 else
@@ -85,9 +101,64 @@ public class LevelGenerator : MonoBehaviour
                 }
                 placement = new Vector3(placement.x + 1.0f, placement.y, 0.0f); // next tile to the right, no change in y value
             }
-            placement = new Vector3(placement.x, placement.y - 1.0f, 0.0f); // next row on the grid, change in y value
+            placement = new Vector3(-5.5f, placement.y - 1.0f, 0.0f); // next row on the grid, change in y value
         }
-        // Mirroring here???
+
+        // Horizontal Mirroring
+        placement = new Vector3(8.5f, 4.5f, 0.0f); // position of top-right corner
+        for (int i = 0; i < 15; i++) // nested for loop to iterate through 2D array
+        {
+            for (int j = 13; j > -1; j--)
+            {
+                if (levelMap[i, j] == 1)
+                {
+                    GameObject outsideCornerClone = Instantiate(outsideCorner, placement, Quaternion.identity);
+                    outsideCornerClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    //outsideCornerClone.transform.Rotate(0.0f, 0.0f, 180.0f);
+                }
+                else if (levelMap[i, j] == 2)
+                {
+                    GameObject outsideWallClone = Instantiate(outsideWall, placement, Quaternion.identity);
+                    outsideWallClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    //outsideWallClone.transform.Rotate(0.0f, 0.0f, 180.0f);
+                }
+                else if (levelMap[i, j] == 3)
+                {
+                    GameObject insideCornerClone = Instantiate(insideCorner, placement, Quaternion.identity);
+                    insideCornerClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    //insideCornerClone.transform.Rotate(0.0f, 0.0f, 180.0f);
+                }
+                else if (levelMap[i, j] == 4)
+                {
+                    GameObject insideWallClone = Instantiate(insideWall, placement, Quaternion.identity);
+                    insideWallClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    //insideWallClone.transform.Rotate(0.0f, 0.0f, 180.0f);
+                }
+                else if (levelMap[i, j] == 5)
+                {
+                    GameObject normalPelletClone = Instantiate(normalPellet, placement, Quaternion.identity); // no rotation
+                    normalPelletClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+                else if (levelMap[i, j] == 6)
+                {
+                    GameObject powerPelletClone = Instantiate(powerPellet, placement, Quaternion.identity); // no rotation
+                    powerPelletClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+                else if (levelMap[i, j] == 7)
+                {
+                    GameObject junctionClone = Instantiate(junction, placement, Quaternion.identity);
+                    junctionClone.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    //junctionClone.transform.Rotate(0.0f, 0.0f, 270.0f);
+                }
+                else
+                {
+                    // Leave space for next tile if element = 0
+                }
+                placement = new Vector3(placement.x + 1.0f, placement.y, 0.0f); // next tile to the right, no change in y value
+            }
+            placement = new Vector3(8.5f, placement.y - 1.0f, 0.0f); // next row on the grid, change in y value
+        }
+
     }
 
     // Update is called once per frame
