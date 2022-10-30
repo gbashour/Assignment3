2a9b0040c35852326234295bee5790c47500026d
Georgette Bashour <13901057@student.uts.edu.au>
2022-10-30 16:46:56 +1100

    Added a working round timer, all UI updates are paused while counting down

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,11 +9,19 @@ public class UIManager : MonoBehaviour
     public TMPro.TextMeshProUGUI score;
     public TMPro.TextMeshProUGUI gameTimer;
     public TMPro.TextMeshProUGUI ghostScaredTimer;
+    public TMPro.TextMeshProUGUI roundTimer;
     float timer;
     int minutes, seconds, milliseconds;
+    int lastTime = 0, countdown = 3;
     /* Text type throws a Null Reference Exception, doesn't register TextMeshPro as type Text */
     public TMPro.TextMeshProUGUI highScore; // after testing, figured out that the type "Text" doesn't work because I'm using TextMeshPro
     public TMPro.TextMeshProUGUI time;
+
+    private static bool roundStart = false;
+    public static bool RoundStart
+    {
+        get { return roundStart; }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,27 +36,48 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* GAME TIMER */
-        timer += Time.deltaTime; // this is in seconds
-        int milliTimer = (int)(timer * 1000.0f); // total milliseconds
-        minutes = milliTimer / 60000; // how many minutes = total milliseconds / 60000
-        seconds = (int)timer - (minutes * 60);
-        milliseconds = (milliTimer - (minutes * 60000) - (seconds * 1000)) / 10;
-
         if (SceneManager.GetActiveScene().buildIndex == 0) // if Level1Scene is the active scene
         {
-            gameTimer.text = "Timer: " + minutes + ":" + seconds + ":" + milliseconds; // update the timer every frame
-            int totalScore = PacStudentController.PacStudentScore + CherryController.PeachScore; // store total score in separate variable because otherwise it just concatenates them like strings
-            score.text = "Score: " + totalScore; // update score every frame
-            //Debug.Log(PacStudentController.PacStudentScore);
-            //Debug.Log(CherryController.PeachScore);
-            if (PacStudentController.GhostTimer < 11 && PacStudentController.GhostTimer > -1)
-            {
-                ghostScaredTimer.text = PacStudentController.GhostTimer + " seconds";
+            /* GAME TIMER */
+            timer += Time.deltaTime; // this is in seconds
+            int milliTimer = (int)(timer * 1000.0f); // total milliseconds
+            minutes = milliTimer / 60000; // how many minutes = total milliseconds / 60000
+            seconds = (int)timer - (minutes * 60);
+            milliseconds = (milliTimer - (minutes * 60000) - (seconds * 1000)) / 10;
 
-            } else
+            if (roundStart)
             {
-                ghostScaredTimer.text = "";
+                gameTimer.text = "Timer: " + minutes + ":" + seconds + ":" + milliseconds; // update the timer every frame
+                int totalScore = PacStudentController.PacStudentScore + CherryController.PeachScore; // store total score in separate variable because otherwise it just concatenates them like strings
+                score.text = "Score: " + totalScore; // update score every frame
+                if (PacStudentController.GhostTimer < 11 && PacStudentController.GhostTimer > -1)
+                {
+                    ghostScaredTimer.text = PacStudentController.GhostTimer + " seconds";
+
+                }
+                else
+                {
+                    ghostScaredTimer.text = "";
+                }
+            }
+
+            if ((int)timer - lastTime == 1) // every second
+            {
+                Debug.Log("I should be printing to the screen");
+                lastTime = (int)timer;
+
+                if (lastTime < 4 && lastTime > 0)
+                {
+                    roundTimer.text = countdown.ToString();
+                    countdown--;
+                } else if (lastTime == 4)
+                {
+                    roundTimer.text = "GO!";
+                } else
+                {
+                    roundTimer.text = "";
+                    roundStart = true;
+                }
             }
         }
     }
@@ -70,6 +99,7 @@ public class UIManager : MonoBehaviour
             gameTimer = GameObject.FindGameObjectWithTag("GameTimer").GetComponent<TMPro.TextMeshProUGUI>();
             score = GameObject.Find("Score").GetComponent<TMPro.TextMeshProUGUI>();
             ghostScaredTimer = GameObject.Find("Ghost Scared Timer").GetComponent<TMPro.TextMeshProUGUI>();
+            roundTimer = GameObject.Find("Round Timer").GetComponent<TMPro.TextMeshProUGUI>();
         }
     }
 
